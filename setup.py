8245e96ce56b78ab1f3a6def98b1d9363c99cdc7
rogpaz <119007074+rogpaz@users.noreply.github.com>
2023-08-29 18:03:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
-DESCRIPTION = 'Teste'
-LONG_DESCRIPTION = 'Teste'
+DESCRIPTION = 'Solver para sistema linear de equações algébricas'
+LONG_DESCRIPTION = 'Use "from Solveq import *" para importar a biblioteca e "exemplo1()" para ver um exemplo de uso'
 
 # Setting up
 setup(
-    name="teste",
+    name="Solveq",
     version=VERSION,
-    author="rogpaz",
+    author=["Rogério","Amanda"],
     author_email="<pazetto1998@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
     install_requires=[],
-    keywords=['python', 'teste', 'test'],
+    keywords=['python', 'equação', 'sistema', 'solver', 'linear'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
